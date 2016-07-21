ee456f315e3ec90d0d457b20d9a43ae64b6fe5c1
MindGamesNl <matsmoolhuizen@gmail.com>
2016-07-21 12:24:25 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,11 @@
 
 //Php zip downloader and extracor thingy by mindgamesnl
 
+define('ENABLE_LOG','true');//use log or not (true is use and false is don't log) stay sassy
+
 define('REPO_ZIP_URL','http://puu.sh/q971Q/9d3cd588e9.zip');//download url for zip/repo
 define('OUTPUTPATH','output/');	//files from zip will be dumped here
+
 define('USERNAME','username here'); //login for bitbucket private repo
 define('PASSWORD','password here');	//login for bitbucket private repo
 
